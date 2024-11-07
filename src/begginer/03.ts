4b0acb427c01ed980a5e1814cddf1137a707be24
yusei53 <yusei530922@gmail.com>
2024-11-07 17:50:20 +0900

    add 03.ts

---
+++ src/begginer/03.ts
@@ -0,0 +1,29 @@
+// 配列の長さを返すLength<T>を実装してください。
+
+type tesla = ["tesla", "model 3", "model X", "model Y"];
+type spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT"
+];
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Length<T extends any[]> = T["length"];
+
+/*
+ * Tにteslaが入ってきた場合、T["length"] = tesla["length"] = 4になる
+ * 型["length"] で配列の長さを取得できる
+ 
+ * extends any[] で配列のみを受け付けるようにしている
+ * extends any[] がないと、Length<number> などが通ってしまうが、受け付けてるのは配列のみなのでコンパイルエラーしてくれる
+ */
+
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
+// type personLength = Length<Person>; → expected error

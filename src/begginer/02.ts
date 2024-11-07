8127fafebc8c1cac2b0fa5a693521bb72e1d976f
yusei53 <yusei530922@gmail.com>
2024-11-07 17:04:05 +0900

    add 02.ts

---
+++ src/begginer/02.ts
@@ -0,0 +1,29 @@
+// MyReadonly<T>を実装してください
+// MyReadonlyは、Tのプロパティをすべて読み取り専用にする型です
+
+type Todo = {
+  title: string;
+  description: string;
+};
+
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K];
+  // readonly[K in keyof T] = "title" → Todo["title"]
+};
+
+/*  
+  * TにTodoが入ってきた場合、[K in keyof T]には"title"と"description"が入る
+  * T[K] = Todo["title"] = stringになり、結果的に
+  
+  * readonly [K in keyof T]: T[K] = 
+  * readonly "title": string; 
+  * readonly "description": string; 
+  
+  * となる
+
+*/
+
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar",
+};

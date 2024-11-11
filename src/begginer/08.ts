81c02f3a4bdd5dc53bb66fd0cf077de3b4e17cbf
yusei53 <yusei530922@gmail.com>
2024-11-11 10:30:21 +0900

    add 08.ts

---
+++ src/begginer/08.ts
@@ -0,0 +1,6 @@
+// type Ifを実装してください。
+
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+type A = If<true, "a", "b">; // expected to be 'a'
+type B = If<false, "a", "b">; // expected to be 'b'
